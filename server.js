aa6966a797fb55f51f8b91a4d18665120147da32
lexyoff1 <lexytb4005botdev@gmail.com>
2026-09-12 00:34:11 -0400

    Update server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ Sujet : ${ticket.sujet}
 Auteur : ${ticket.auteur}
 
 Lien :
-http://localhost:3000/tickets-commandement.html?id=${ticket.id}`
+https://gendarmerie-bordeauxrp.onrender.com/tickets-commandement.html?id=${ticket.id}`
     );
 }
 
